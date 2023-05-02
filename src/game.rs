b6e05de969b6df0749c5a3c11c8731371aa307b8
Tim Patterson <tpatterson@sailthru.com>
2023-05-03 11:12:46 +1200

    First commit

---
+++ src/game.rs
@@ -0,0 +1,115 @@
+#[derive(Debug)]
+pub struct Player {
+    name: String,
+    cookie: String
+}
+
+#[derive(Debug)]
+pub struct PlayerIdx(usize);
+
+#[derive(Debug)]
+pub struct Game {
+    players: Vec<Player>,
+    board: Board,
+    state: GameState
+}
+
+#[derive(Debug)]
+pub enum GameState {
+    WaitingForPlayers,
+    Playing(PlayingState),
+    Finished(FinishedState)
+}
+
+#[derive(Debug)]
+pub struct PlayingState {
+    next_turn: PlayerIdx,
+    die_order: Vec<PlayerIdx>
+}
+
+#[derive(Debug)]
+pub struct FinishedState {
+    die_order: Vec<PlayerIdx>
+}
+
+#[derive(Debug)]
+pub struct Board {
+    height: i32,
+    width: i32,
+    board: Vec<Vec<BoardCell>>
+}
+
+#[derive(Debug)]
+pub struct BoardCell {
+    uncovered: bool,
+    ship_nodes: Vec<PlayerIdx>
+}
+
+impl Game {
+    pub fn new(height: i32, width: i32) -> Self {
+        Game {
+            players: vec![],
+            board: Board::new(height, width),
+            state: GameState::WaitingForPlayers,
+        }
+    }
+
+    pub fn add_player(&mut self, name: &str, cookie: &str) {
+        if let GameState::WaitingForPlayers = &self.state {
+            self.players.push(Player {
+                name: name.to_string(),
+                cookie: cookie.to_string(),
+            })
+        } else {
+            println!("Tried to add new player while not in the waiting for players state");
+        }
+    }
+    
+    pub fn start_game(&mut self) {
+        if let GameState::WaitingForPlayers = &self.state {
+            if self.players.len() >= 2 {
+                self.state = GameState::Playing(PlayingState {
+                    next_turn: PlayerIdx(0),
+                    die_order: vec![],
+                })
+            }
+        } else {
+            println!("Tried to start the game when its already been started");
+        }
+    }
+}
+
+impl Board {
+    fn new(height: i32, width: i32) -> Self {
+        let board = (0..width).into_iter().map(|_| {
+            (0..height).into_iter().map(|_| BoardCell::new()).collect()
+        }).collect();
+
+        Board {
+            height,
+            width,
+            board,
+        }
+    }
+}
+
+impl BoardCell {
+    fn new() -> Self {
+        BoardCell {
+            uncovered: false,
+            ship_nodes: vec![],
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::game::*;
+
+    #[test]
+    fn test_new_game() {
+        let mut game = Game::new(10, 5);
+        game.add_player("Tim", "1234");
+        assert_eq!(game.board.height, 10);
+    }
+}
